2e18a673bf843fdb7affdfeba86c3b6ec5f712b5
mario_chino98 <mario.martinez@accedo.tv>
2022-03-02 15:56:43 -0600

    refactor: Delete unused imports

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 import colors from '../../theme/colors';
 import githubLogo from '../../static/github-icon.png'
